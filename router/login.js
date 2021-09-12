aaf7cbb8d823db5ad89ad11e846c4db63922a602
Spartak-javohir <dellspartak0@gmail.com>
2021-09-12 17:19:33 +0500

    change

--- router/login.js
+++ router/login.js
@@ -1,10 +1,43 @@
 const router = require("express").Router();
 
-router.post('/', async (req, res)=>{
-    req.body.users.findOne({
-        email: email.toLowerCase()
-    })
-})
+router.post("/login", async (req, res) => {
+	const {
+		email,
+		password
+	} = req.body;
+	const users = await req.db.users
+
+	if (!(email && password)) {
+		res.render("index", {
+			error: "Email or Password not found",
+		});
+		return;
+	}
+
+	let user = await users.findOne({
+		email: email.toLowerCase(),
+	});
+
+	if (!user) {
+		res.render("index", {
+			error: "User not found",
+		});
+		return;
+	}
+
+	if (!(await compareCrypt(user.password, password))) {
+		res.render("index", {
+			error: "Password is incorrect",
+		});
+		return;
+	}
+
+	const token = createToken({
+		user_id: user._id,
+	});
+
+	res.cookie("token", token).redirect("/profile");
+});
 
 module.exports = {
     path: "/login",
