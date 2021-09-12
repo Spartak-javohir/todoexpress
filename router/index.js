320d4afb0916d4cad5963756f28e91e46fb1816c
Spartak-javohir <dellspartak0@gmail.com>
2021-09-12 12:50:39 +0500

    change Index.ejs

--- router/index.js
+++ router/index.js
@@ -1,5 +1,14 @@
+const HomeLoginPostController = require("../controllers/HomeLoginPostController");
+const HomeSignUpPostController = require("../controllers/HomeSignUpPostController");
+
+
 const router = require("express").Router();
 
 router.get(["/", "/index.html"], (req, res) => {
 	res.render("index");
 });
+
+module.exports = {
+	router,
+	path: "/",
+};
