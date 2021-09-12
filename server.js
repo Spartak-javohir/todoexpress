5a73c2eccb30b747b8c8da87d795cb29dbcc05f3
Spartak-javohir <dellspartak0@gmail.com>
2021-09-12 11:20:19 +0500

    install cookie-parser

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const server = express();
 const PORT = process.env.PORT || 8888;
-// const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const mongodb = require("./modules/mongo");
 const routes = require("./router/routes");
