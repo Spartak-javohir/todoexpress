454fc7c29ba39c2dcb289826c2c65895391a9260
Spartak-javohir <dellspartak0@gmail.com>
2021-09-12 18:30:01 +0500

    change

--- router/signup.js
+++ router/signup.js
@@ -2,14 +2,16 @@ const router = require("express").Router();
 router.get("/register", (req, res) => {
 	res.render("register");
 });
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
 	console.log();
 	const {
 		email,
 		password,
 		name
-	} = req.body;
-	const users = await req.db.users
+	} = req.body; 
+
+const users = req.db.users
+
 	if (!(email && password&& name)) {
 		res.render("register", {
 			error: "Email or Password not found",
@@ -31,12 +33,13 @@ router.post("/", async (req, res) => {
 	user = await users.insertOne({
 		email: email.toLowerCase(),
 		password: await createCrypt(password),
+		name: name
 	});
 
 	res.redirect("/");
 });
 
 module.exports = {
-    path: "/singup",
+    path: "/",
     router
 }
