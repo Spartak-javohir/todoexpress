a24dc70d694ee9900a8bed0f442a77dcad2f81a8
Spartak-javohir <dellspartak0@gmail.com>
2021-09-13 16:17:15 +0500

    push

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,19 +1,9 @@
 module.exports = async function ProfileController(req, res) {
 	try {
 
-		let user = await req.db.users.findOne({
-			email: data.email.toLowerCase(),
-		});
-        let data = await req.body
-
-		user = await req.db.usersinfo.insertOne({
-			...data,
-
-		});
-
-		res.redirect("/login.html");
+		console.log(res.cookie());
 	} catch (error) {
-		res.render("register", {
+		res.render("index", {
 			error,
 		});
 	}
