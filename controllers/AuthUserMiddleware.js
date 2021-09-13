6ce132d1ce3a3b4034b064f79ed71332f3ef8f15
Spartak-javohir <dellspartak0@gmail.com>
2021-09-13 19:07:31 +0500

    change

--- controllers/AuthUserMiddleware.js
+++ controllers/AuthUserMiddleware.js
@@ -3,4 +3,22 @@ module.exports= async function AuthUserMiddleware(req, res, next) {
     if(!req.cookies.token) {
         res.redirect("/");
     }
+    if (!user) throw new Error("User not found");
+
+    const isTrust = await compareHash(data.password, user.password);
+
+
+    if (!isTrust) throw new Error("Password is incorrect");
+
+    const token = await createToken({
+        id: user._id,
+    });
+    if(!req.cookies.token) {
+        res.redirect("/");
+    }
+    let profile = await req.db.usersinfo.insertOne({
+        email: data.email,
+    });
+
+    res.cookie("token", token).redirect("/profile");
 }
