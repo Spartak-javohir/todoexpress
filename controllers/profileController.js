18d154167a52e901cb0d1b4e061169b52dcfbd3d
Spartak-javohir <dellspartak0@gmail.com>
2021-09-14 10:31:19 +0500

    change db profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,9 +1,24 @@
+
+
 module.exports = async function ProfileController(req, res) {
 	try {
 	const {user_id} =req.user
-		console.log(
-			user_id
-		);
+		let {todotext}=req.body
+	await req.db.users.updateOne({
+			_id: ObjectId(user_id),
+			todotext: todotext
+		},{
+			$push:{
+				todotext: {
+					$each: [{
+						todotext: req.body.todotext,
+						time: new Date().toLocaleString()
+					}]
+				}
+			}
+		})
+
+		res.redirect('/profile')
 		
 	} catch (error) {
 		res.render("index", {
