45b8753eb170a2165feac151bdb199713d0e196c
Spartak-javohir <dellspartak0@gmail.com>
2021-09-15 07:36:52 +0500

    ochistit

--- router/index.js
+++ router/index.js
@@ -64,15 +64,6 @@ router.get('/delete/:time',AuthUserMiddleware, async(req, res)=>{
 			e.todotext.filter(filterad)
 		}
 	});
-	
-
-	// let text = await info.findOne({todotext:req.params.todotext})
-	// console.log(todotexts.findOne({todotext: req.params.todotext}));
-	// req.db.users.deleteOne(todotext)
-	// let text = await req.db.users.findIndex(e=>{
-	// 	e.todotext=req.params.todotext
-	// })
-	// req.db.users.updateOne(text)
 
 	res.redirect("/profile")
 
