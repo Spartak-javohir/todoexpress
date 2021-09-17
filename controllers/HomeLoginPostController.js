142090a3e75511ce9042074dfef8d68b90373419
Spartak-javohir <dellspartak0@gmail.com>
2021-09-17 16:38:34 +0500

    change

--- controllers/HomeLoginPostController.js
+++ controllers/HomeLoginPostController.js
@@ -29,7 +29,7 @@ module.exports = async function HomeLoginPostController(req, res) {
 		
 		
 		res.cookie("token", token).redirect("/profile");
-		
+																																												
 		
 		
 	} catch (error) {
